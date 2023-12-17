168bd44ceb647b9060e327756c59514ca9107400
Leonardo <leo321war@gmail.com>
2023-12-17 16:50:42 -0500

    Uploaded project to GitHub

---
+++ src/pataAmarilla/PataAmarilla.java
@@ -0,0 +1,14 @@
+package pataAmarilla;
+
+public class PataAmarilla {
+    
+    public static void main(String[] args) {
+              
+        Login ventanaLog = new Login();
+        ventanaLog.setVisible(true);
+        ventanaLog.setLocationRelativeTo(null);
+        
+        
+    }
+    
+}
